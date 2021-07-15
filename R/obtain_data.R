08e1cb69c4db085e0f83d48114794ea599b1656a
hezibu <hezibuba@mail.tau.ac.il>
2021-07-15 11:34:55 +0300

    Latest Run

--- R/obtain_data.R
+++ R/obtain_data.R
@@ -83,7 +83,7 @@ source("R/reading_functions.R")
 #   Select an expedition folder under Data Sheets in Google Drive
 #   and the folder of today's sampling.
 
-this_expedition <- "Tel Aviv Project"
+this_expedition <- "EcoCamp June 2021"
 todays_folder   <- "Tel Aviv 2021-06-01"
 
 #######  Step 4: Download the data #############
